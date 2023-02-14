22d1833dc59ffa9d483a687b4bae7d1a26d5d4c6
Allassane <dickoallassane1997@gmail.com>
2023-02-14 16:35:37 +0100

    Add navMain structure and styling

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,7 +3,7 @@ import DetailInfo from '../../components/main/DetailInfo/DetailInfo'
 import ListChats from '../../components/main/ListChats/ListChats'
 import ListFriends from '../../components/main/ListFriends/ListFriends'
 import ListUsers from '../../components/main/ListUsers/ListUsers'
-import NavBarMain from '../../components/main/NavBarMain/NavBarMain'
+import NavMain from '../../components/main/NavMain/NavMain'
 import Profile from '../../components/main/Profile/Profile'
 import './main.css'
 
@@ -11,7 +11,7 @@ const Main = () => {
   return (
     <div className='flex-center main'>
       <main className="column wrapper">
-        <NavBarMain />
+        <NavMain />
         <div className='main-side'>
           <ListFriends />
           <Profile />
