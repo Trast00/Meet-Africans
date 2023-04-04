ea99026415a7d9deebea022a00ccf2c686ae05f9
Allassane <dickoallassane1997@gmail.com>
2023-04-04 17:28:28 +0100

    Add faceEmojies

--- src/components/main/ListChats/ListEmojie/ListEmoji.jsx
+++ src/components/main/ListChats/ListEmojie/ListEmoji.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import Emoji from 'react-emoji-render';
 import { Twemoji } from 'react-emoji-render';
+import styles from './ListEmoji.module.css';
+
+/* TODO:
+- style emojie
+- Add emojie animation
+- Add emojie menu open/close animation
+- reference Twemoji
+*/
 
 const ListEmoji = () => {
-  const smileysAndEmotions = [":)", ":-)", ":D", ":P", ";)", ":*", "<3", ":O", ":|", ":(", ":'(", ":@", ":#", ":$", ":&", ":X", ":S", ":/", ":\\", ":sick:", ":woozy_face:", ":exploding_head:", ":crazy_face:", ":smiling_face_with_3_hearts:"];
+  const smileysAndEmotions = [":)", ":-)", ":D", ":P", ";)", ":*", "<3", ":O", ":|", ":(", ":'(", ":@", ":$", ":/", ":sick:", ":woozy_face:", ":exploding_head:", ":smiling_face_with_3_hearts:", ":face_with_tears_of_joy:", ":rolling_on_the_floor_laughing:", ":smiling_face_with_sunglasses:", ":smiling_face_with_smiling_eyes:", ":smiling_face_with_heart-eyes:", ":face_blowing_a_kiss:", ":kissing_face:", ":kissing_face_with_closed_eyes:", ":kissing_face_with_smiling_eyes:", ":smiling_face_with_halo:", ":smiling_face_with_horns:", ":angry_face:", ":pouting_face:", ":face_with_raised_eyebrow:", ":face_with_monocle:", ":thinking_face:", ":zipper-mouth_face:", ":nauseated_face:", ":face_with_thermometer:", ":face_with_head-bandage:", ":face_with_rolling_eyes:", ":face_with_hand_over_mouth:", ":face_savoring_food:", ":face_with_tongue:"];
   const peopleAndBody = [
     ":baby:", ":boy:", ":girl:", ":man:", ":woman:", ":older_man:",
     ":older_woman:", ":cop:", ":construction_worker:", ":guardsman:",
@@ -19,11 +26,11 @@ const ListEmoji = () => {
     ":horse:", ":unicorn:", ":turtle:"
   ];
   return (
-    <div className='max-width'>
-      <p>List emojies:</p>
-      <ul className='list-emojies'>
-        {smileysAndEmotions.map(emoji => (
-          <li><Twemoji text={emoji}/></li> 
+    <div className={styles.container}>
+      <p className={styles.category}>List emojies:</p>
+      <ul className={styles.emojis}>
+        {smileysAndEmotions.map((emoji, index) => (
+          <li key={index}><Twemoji text={emoji} className={styles.emoji} /></li> 
         ))}
       </ul>
     </div>
